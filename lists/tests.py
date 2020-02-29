edf768c1de8cbe6418b08e6b667b473e8d603409
jovce <jovce10@gmail.com>
2020-02-29 20:53:47 +0100

    new urls, moved code in views

--- lists/tests.py
+++ lists/tests.py
@@ -10,7 +10,6 @@ from lists.models import Item
 
 class HomePageTest(TestCase):
 
-
     def test_home_page_returns_correct_html(self):
         response  = self.client.get('/')
         html = response.content.decode('utf8')
@@ -20,25 +19,6 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
 
         self.assertTemplateUsed(response, 'home.html')
-
-
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text':'A new list item'})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirect_after_POST(self):
-        response = self.client.post('/', data={'item_text':'A new list item'})
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-    
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
@@ -76,3 +56,15 @@ class LiveViewTest(TestCase):
         # assertContains know how to deal with bytes, so no decode is required
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text':'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirect_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text':'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
